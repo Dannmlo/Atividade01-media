14a933cab4210a0e2bd7fdca87123215f351daec
Dannmlo <antonio.melo114@aluno.ce.gov.br>
2023-10-10 16:50:33 -0300

    adicionado recurso input, e correção de media

--- media.py
+++ media.py
@@ -2,4 +2,4 @@ nota01=float(input("Digite nota 1:"))
 nota02=float(input("Digite nota 2:"))
 nota03=float(input("Digite nota 3:"))
 media= (nota01+nota02+nota03)/3
-print("Sua média é:", media)
+print ("Sua média é:", media)
